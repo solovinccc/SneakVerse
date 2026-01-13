4948dc9e77770fa4f2f64c2c4fda4f8dde12ff2c
Vincenzo <vincenzolisi12@gmail.com>
2026-01-13 15:47:36 +0100

    Added OrderItem

---
+++ src/main/java/com/vincenzolisi/SneakVerse/repository/OrderItem.java
@@ -0,0 +1,6 @@
+package com.vincenzolisi.SneakVerse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItem extends JpaRepository <OrderItem, Integer> {
+}
